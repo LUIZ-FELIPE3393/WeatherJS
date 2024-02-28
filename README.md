dba1dfd7630b85ca16a6b9e44da226f254ee2aa9
LUIZ-FELIPE3393 <2023003393@ifam.edu.br>
2024-02-28 16:17:32 -0400

    Tentativa de renderizar imagens

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # WeatherJS
  Simple app to read the weather info and display them on Web
 
-1. Fetch the weather data from the API
+1. Fetch the weather data from the API using a URI
 
 2. Process data
+    - Fetch a JSON file with weather data
+    - Extract necessary information
 
-3. Layout the info with images and gifs
+3. Layout the extracted information through a canvas
 
 # USES:
 1. Fetch API
--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-function evaluateCondition(condDisplay, conditionCodes) {
+function evaluateCondition(ctx, condDisplay, conditionCodes) {
     let conArr = conditionCodes.trim().split(',');
     condDisplay.textContent = "COND:"
+    let imgArr = [];
 
     for (let i = 0; i < conArr.length; i++) {
         switch(conArr[i]) {
             case "type_21":
                 condDisplay.textContent = condDisplay.textContent.concat("|", "CHUVA");
+                imgArr[i] = new Image();
+                imgArr[i].src = "img/rainy.png";
             case "type_41":
                 condDisplay.textContent = condDisplay.textContent.concat("|", "NUBLADO");
+                imgArr[i] = new Image();
+                imgArr[i].src = "img/cloudy.png";
         }
     }
-
     console.log(condDisplay.textContent);
+    return imgArr;
 }
 
 async function getWeatherJSON() {
@@ -19,7 +24,7 @@ async function getWeatherJSON() {
         const response = 
         await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" +
             "MANAUS/today?include=fcst%2Cobs%2Chistfcst%2Cstats%2Cdays%2Chours&" +
-            "key=Y3KFJPKUUX6S8CX84GSXY9VJY&contentType=json&lang=id&unitGroup=metric", { // Here goes the URL of the API
+            "key=Y3KFJPKUUX6S8CX84GSXY9VJY&contentType=json&lang=id&unitGroup=metric", { // Here goes the URI of the API
             "method": "GET",
             "headers":{}});
         
@@ -38,11 +43,14 @@ async function getWeatherJSON() {
 const tempDisplay = document.querySelector('.temp');
 const condDisplay = document.querySelector('.condTxt');
 const canvas = document.getElementById("condCnv");
+let ctx = canvas.getContext("2d");
 
 getWeatherJSON().then((res) => {
-    console.log(res.days[0].temp);
-    evaluateCondition(condDisplay, res.days[0].conditions);
+    let img = evaluateCondition(ctx, condDisplay, res.days[0].conditions);
     tempDisplay.textContent = "TEMP MIN: " + res.days[0].tempmin + "°C TEMP MAX: " + res.days[0].tempmax + "°C";
+
+    for (let i = 0; i < img.length; i++) {
+        ctx.drawImage(img[i], 10, 10);
+        console.log("Imagem:" + img[i]);
+    }
 });
-
-

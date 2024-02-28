8fe1284281b1db66bb85744dc3da8688120eda4c
LUIZ-FELIPE3393 <2023003393@ifam.edu.br>
2024-02-28 15:38:38 -0400

    Exibição da Condição do Clima

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
+function evaluateCondition(condDisplay, conditionCodes) {
+    let conArr = conditionCodes.trim().split(',');
+    condDisplay.textContent = "COND:"
+
+    for (let i = 0; i < conArr.length; i++) {
+        switch(conArr[i]) {
+            case "type_21":
+                condDisplay.textContent = condDisplay.textContent.concat("|", "CHUVA");
+            case "type_41":
+                condDisplay.textContent = condDisplay.textContent.concat("|", "NUBLADO");
+        }
+    }
+
+    console.log(condDisplay.textContent);
+}
+
 async function getWeatherJSON() {
     try {
         const response = 
-        await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/" +
-        "rest/services/timeline/Manaus/2024-02-27T16:00:00?key=Y3KFJPKUUX6S8CX84GSXY9VJY", { // Here goes the URL of the API
+        await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" +
+            "MANAUS/today?include=fcst%2Cobs%2Chistfcst%2Cstats%2Cdays%2Chours&" +
+            "key=Y3KFJPKUUX6S8CX84GSXY9VJY&contentType=json&lang=id&unitGroup=metric", { // Here goes the URL of the API
             "method": "GET",
             "headers":{}});
         
         if (!response.ok) {
             throw new Error("ERROR - HTTP response code isn't ok");
         }
-        console.log("SUCCESS - Weather data fetched:", response);
         const weather = await response.json();
+        console.log("SUCCESS - Weather data fetched:", weather);
         return weather;
 
     } catch (error) {
@@ -18,11 +35,14 @@ async function getWeatherJSON() {
     }
 }
 
-const weatherDisplay = document.querySelector('.weatherDisplay');
+const tempDisplay = document.querySelector('.temp');
+const condDisplay = document.querySelector('.condTxt');
+const canvas = document.getElementById("condCnv");
 
 getWeatherJSON().then((res) => {
     console.log(res.days[0].temp);
-    weatherDisplay.textContent = "TEMP: " + res.days[0].temp + " MIN: " + res.days[0].tempmin + " MAX: " + res.days[0].tempmax;
+    evaluateCondition(condDisplay, res.days[0].conditions);
+    tempDisplay.textContent = "TEMP MIN: " + res.days[0].tempmin + "°C TEMP MAX: " + res.days[0].tempmax + "°C";
 });
 
 
